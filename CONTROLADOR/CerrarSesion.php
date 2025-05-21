c2b9b991dc2cf9cc05617e8958e377261160bed5
Yovactt <221u0256@alumno.itssat.edu.mx>
2025-05-21 00:12:57 -0600

    Actualizar

--- CONTROLADOR/CerrarSesion.php
+++ CONTROLADOR/CerrarSesion.php
@@ -2,5 +2,5 @@
 session_start();
 session_unset(); // Limpia las variables de sesión
 session_destroy(); // Destruye la sesión
-header("Location: ../INDEX.php");
+header("Location: ../index.php");
 exit();
