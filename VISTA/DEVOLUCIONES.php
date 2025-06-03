054619aa3f5b588972bd496cd11db09f911d07fd
Yovactt <221u0256@alumno.itssat.edu.mx>
2025-06-03 13:19:30 -0600

    Update DEVOLUCIONES.php

--- VISTA/DEVOLUCIONES.php
+++ VISTA/DEVOLUCIONES.php
@@ -507,7 +507,7 @@
     </path>
   </svg>
 <script>
-  const urlBase = 'Devoluciones.php';
+  const urlBase = '/CONTROLADOR/Devoluciones.php';
 
   // Evento para cuando presionas Enter en el input etiqueta
   document.getElementById("etiqueta").addEventListener("keypress", function(e) {
